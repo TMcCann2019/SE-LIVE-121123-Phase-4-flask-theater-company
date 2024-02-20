c58191e43282a9f2cfaf51f0d8ca5730646b4e62
Tim McCann <timothysmccann@gmail.com>
2024-02-20 15:02:26 -0500

    completed day 1 lecture

--- 01-intro-to-flask/server/models.py
+++ 01-intro-to-flask/server/models.py
@@ -9,7 +9,22 @@ from flask_sqlalchemy import SQLAlchemy
      
 db = SQLAlchemy()
 # 1. ✅ Create a Production Model
+class Production(db.Model):
 	# tablename = 'Productions'
+    __tablename__ = 'productions'
 	# Columns:
-        # title: string, genre: string, budget:float, image:string,director: string, description:string, ongoing:boolean, created_at:date time, updated_at: date time 
+        # title: string, genre: string, budget:float, image:string,director: string, description:string, ongoing:boolean, created_at:date time, updated_at: date time
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    genre = db.Column(db.String)
+    budget = db.Column(db.Float)
+    image = db.Column(db.String)
+    director = db.Column(db.String)
+    description = db.Column(db.String)
+    ongoing = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime, server_default=db.func.now()) #considered best practice to do this for records
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now()) #considered best practice to do this for records
+
+    def __repr__(self):
+        return f'<Production | Title: {self.title} | Genre: {self.genre} | Budget: {self.budget} | Director: {self.director} | Description: {self.description} | Ongoing: {self.ongoing}'
 # 2. ✅ navigate to app.py
--- 01-intro-to-flask/server/seed.py
+++ 01-intro-to-flask/server/seed.py
@@ -4,22 +4,80 @@
 # 5. ✅ Imports
     # app from app
     # db and Production from models
+from app import app
+from models import db, Production
 
 # 6. ✅ Initialize the SQLAlchemy instance with `db.init_app(app)`
 
-
-# 7. ✅ Create application context `with app.app_context():`
-    # Info on application context: https://flask.palletsprojects.com/en/1.1.x/appcontext/
-
 # 8.✅ Create a query to delete all existing records from Production    
-   
-# 9.✅ Create some seeds for production and commit them to the database. 
+def clear_database():
+    print("Clearing the database...")
+    Production.query.delete()
+
+# 9.✅ Create some seeds for production and commit them to the database.
+    
+def create_productions():
+    print("Creating productions...")
+    productions = []
+
+    p1 = Production(
+        title = "Hamlet",
+        genre= "Drama",
+        budget = 10000,
+        director = "Bill S. Peare",
+        description = "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+        ongoing = True
+    )
+
+    productions.append(p1)
+
+    p2 = Production(
+        title = "Cats",
+        genre= "Musical",
+        budget = 20000,
+        director = "Andrew Lloyd Webber",
+        description = "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+        ongoing = True
+    )
+
+    productions.append(p2)
+
+    p3 = Production(
+        title = "Carmen",
+        genre= "Opera",
+        budget = 20000,
+        director = "Georges Bizet",
+        description = "blah",
+        ongoing = True
+    )
+
+    productions.append(p3)
+
+    p4 = Production(
+        title = "Hamilton",
+        genre= "Musical",
+        budget = 40000,
+        director = "Lin-Manuel Miranda",
+        description = "blah blah",
+        ongoing = True
+    )
+
+    productions.append(p4)
+
+    db.session.add_all(productions)
+    db.session.commit()
+
 # 10.✅ Run in terminal:
-    # `python seed.py`
+# `python seed.py`
 # 11.✅ run `flask shell` in the terminal 
-    # from app import app
-    # from models import Production
-    # Check the seeds by querying Production
+# from app import app
+# from models import Production
+# Check the seeds by querying Production
 # 12.✅ Navigate back to app.py  
-    
-    
+
+if __name__ == '__main__':
+    # 7. ✅ Create application context `with app.app_context():`
+    # Info on application context: https://flask.palletsprojects.com/en/1.1.x/appcontext/
+    with app.app_context():
+        clear_database()
+        create_productions()
